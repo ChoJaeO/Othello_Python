09984675aaf5b5fc3a92bbc2347f14d1e3a32066
ChoJaeO <39540532+ChoJaeO@users.noreply.github.com>
2018-12-02 00:37:32 +0900

    게임 구성

---
+++ change.py
@@ -0,0 +1,19 @@
+class Change:
+    def __init__(self):
+        pass
+
+    def changestones(self, stone_location):	#stone_location : 흑돌 백돌 좌표값
+        #흑돌 백돌 뒤집는 메서드
+        pass
+
+    def getresult(self):
+        #결과값 반환 메서드 - 리스트 형태(좌표 형태)
+        pass
+
+    def getblackcnt(self):
+        #흑돌 개수 반환 메서드
+        pass
+
+    def getwhitecnt(self):
+        #백돌 개수 반환 메서드
+        pass
---
+++ undogame.py
@@ -0,0 +1,12 @@
+class UndoTurn:
+    def __init__(self):
+        pass
+
+    def addstatus(self, new_status):	#new_status : 최근 게임 판
+        #최근 게임 판 추가
+        pass
+
+    def getstatus(self):
+        #이전 판 반환
+        #return status.pop()
+        pass
