f4614ac46e2e1ec3e0fc62767e9c821eace69674
alsgur3480 <alsgur3480@kookmin.ac.kr>
2018-12-11 11:07:54 +0900

    코드 간략화 및 버그 수정

--- undogame.py
+++ undogame.py
@@ -1,6 +1,8 @@
 class UndoTurn:
     def __init__(self):
-        self.game_status = [[[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 2, 0, 0, 0], [0, 0, 0, 2, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]],]
+        self.i = 0
+        self.game_status = [0 for i in range(64)]
+        self.game_status[0] = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 2, 0, 0, 0], [0, 0, 0, 2, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
         self.cnt_status = [[2, 2],]
 
     def setstatus(self, new_status):
@@ -8,14 +10,22 @@ class UndoTurn:
 
     def addstatus(self, new_status):	#new_status : 최근 게임 판
         #최근 게임 판 추가
-        self.game_status += [new_status]
-        print(self.game_status)
+        self.i += 1
+        print(self.i)
+        print("newstatus")
+        print(new_status)
+        self.game_status[self.i] = new_status
+        print(self.game_status[0])
+        print(self.game_status[1])
+        print(self.game_status[2])
 
     def getstatus(self):
         #이전 판 반환
-        self.game_status = self.game_status[:-1]
-        print(self.game_status)
-        return self.game_status[-1]
+        #self.game_status = self.game_status[:-1]
+        #print(self.game_status)
+        #return self.game_status[-1]
+        self.i -= 1
+        return self.game_status[self.i]
 
     def addcntstatus(self, new_cnt_status):
         print(new_cnt_status)
